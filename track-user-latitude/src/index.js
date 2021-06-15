30058bebdd7babc6a5beae9c17d8e291f7c55b17
sakthikarthickraja98 <60427728+sakthikarthickraja98@users.noreply.github.com>
2021-06-15 16:11:28 +0530

    first commit

---
+++ track-user-latitude/src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDom from "react-dom";
+import "jquery";
+import "popper.js/dist/umd/popper";
+import "bootstrap/dist/js/bootstrap";
+import "bootstrap/dist/css/bootstrap.css";
+import App from "./app";
+
+ReactDom.render(<App/>, document.getElementById("root"));
