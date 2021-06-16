23e700ba8ab8f7185d32b7107a34d930bad45397
sakthikarthickraja98 <60427728+sakthikarthickraja98@users.noreply.github.com>
2021-06-16 13:25:23 +0530

    update icon

--- track-user-latitude/src/seasondisplay.jsx
+++ track-user-latitude/src/seasondisplay.jsx
@@ -11,11 +11,13 @@ const getSeason = (lat, month) =>{
 
 const Season = props => {
     const seas = getSeason(props.lat, new Date().getMonth());
-      const text = seas === 'winter'? 'Burr, it is cjilly':'Lets hit the beach';
-
+    const text = seas === 'winter'? 'Burr, it is cjilly':'Lets hit the beach';
+    const icon = seas === 'winter'? 'snowflake':'sun';
     return (
     <div>
-        {text}
+        <i className={`${icon} icon`}/>
+        <h1>{text}</h1>
+        <i className={`${icon} icon`}/>
     </div>
     );
 }
